27a00bcfcbd3f8c4cb4d2ae7ebf67c32711f886c
captain <moneyboat@foxmail.com>
2018-07-10 20:35:41 +0800

    update code

---
+++ test.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+
+"""
+@Author  : captain
+@time    : 18-7-10 下午8:27
+@ide     : PyCharm  
+"""
+
+import torch
+import torch.optim as optim
+import torch.nn.functional as F
+import time
+import models
+import util
+import opts
+
+opt = opts.parse_opt()
+train_iter, val_iter, test_iter = util.load_data()
+opt.lstm_layers = 2
+
+model = models.setup(opt)
+if torch.cuda.is_available():
+    model.cuda()
+print("using model {}".format(opt.model))
+model.train()
+print("# parameters:", sum(param.numel() for param in model.parameters() if param.requires_grad))
+optimizer = optim.Adam(filter(lambda p: p.requires_grad, model.parameters()), lr=opt.learning_rate)
+loss_funtion = F.cross_entropy
+
+for i in range(opt.max_epoch):
+    for epoch, batch in enumerate(train_iter):
+        optimizer.zero_grad()
+        start = time.time()
+        text = batch.text[0]
+        predicted = model(text)
+
+        loss = loss_funtion(predicted, batch.label)
+        loss.backward()
+        # utils.clip_gradient(optimizer, opt.grad_clip)
+        optimizer.step()
+        if epoch % 100 == 0:
+            if torch.cuda.is_available():
+                loss_val = loss.cpu().data.numpy()
+            else:
+                loss_val = loss.data.numpy()
+            print("%d iteration %d epoch with loss : %.5f in %.4f seconds" % (
+                i, epoch, loss_val, time.time() - start))
+
+    # accuracy = utils.evaluation(model, test_iter)
+    # print("%d iteration with accuracy %.7f" % (i, accuracy))
--- util.py
+++ util.py
@@ -31,31 +31,33 @@ class GrandDataset(data.Dataset):
         super(GrandDataset, self).__init__(examples, fields, **kwargs)
 
 
-TEXT = data.Field(sequential=True, fix_length=500)
-LABEL = data.Field(sequential=False, use_vocab=False)
-# load data
-train_path = '/data/yujun/datasets/daguanbei_data/new_train_set.csv'
-val_path = '/data/yujun/datasets/daguanbei_data/val_set.csv'
-test_path = '/data/yujun/datasets/daguanbei_data/test_set.csv'
-train = GrandDataset(train_path, text_field=TEXT, label_field=LABEL)
-val = GrandDataset(val_path, text_field=TEXT, label_field=LABEL)
-test = GrandDataset(test_path, text_field=TEXT, label_field=None, test=True)
+def load_data():
+    TEXT = data.Field(sequential=True, fix_length=500)
+    LABEL = data.Field(sequential=False, use_vocab=False)
+    # load data
+    train_path = '/data/yujun/datasets/daguanbei_data/new_train_set.csv'
+    val_path = '/data/yujun/datasets/daguanbei_data/val_set.csv'
+    test_path = '/data/yujun/datasets/daguanbei_data/test_set.csv'
+    train = GrandDataset(train_path, text_field=TEXT, label_field=LABEL)
+    val = GrandDataset(val_path, text_field=TEXT, label_field=LABEL)
+    test = GrandDataset(test_path, text_field=TEXT, label_field=None, test=True)
 
-# # 构建Vocub
-# w2v_path = 'emb-100.bin'
-# print('loading word2vec {}'.format(w2v_path))
-# w2v = word2vec.load(w2v_path)
-# # build the vocabulary
-#
-# vectors = Vectors(name='word2vec', cache='.vector_cache/', unk_init=w2v.vectors)
-# TEXT.build_vocab(train, vectors=w2v.vectors)
+    # # 构建Vocub
+    # w2v_path = 'emb-100.bin'
+    # print('loading word2vec {}'.format(w2v_path))
+    # w2v = word2vec.load(w2v_path)
+    # # build the vocabulary
+    #
+    # vectors = Vectors(name='word2vec', cache='.vector_cache/', unk_init=w2v.vectors)
+    # TEXT.build_vocab(train, vectors=w2v.vectors)
 
+    # 构建Iterator
+    # train_iter = data.Iterator(dataset=train, batch_size=32, train=True, repeat=False,
+    #                                device=0 if using_gpu else -1)
+    # # 在 test_iter , sort一定要设置成 False, 要不然会被 torchtext 搞乱样本顺序
+    # test_iter = data.Iterator(dataset=test, batch_size=64, train=False, sort=False, device=0 if using_gpu else -1)
+    train_iter = data.Iterator(dataset=train, batch_size=64, train=True, repeat=False, device=0)
+    val_iter = data.Iterator(dataset=val, batch_size=64, train=False, repeat=False, device=0)
+    test_iter = data.Iterator(dataset=test, batch_size=64, train=False, sort=False, device=0)
 
-# 构建Iterator
-# train_iter = data.Iterator(dataset=train, batch_size=32, train=True, repeat=False,
-#                                device=0 if using_gpu else -1)
-# # 在 test_iter , sort一定要设置成 False, 要不然会被 torchtext 搞乱样本顺序
-# test_iter = data.Iterator(dataset=test, batch_size=64, train=False, sort=False, device=0 if using_gpu else -1)
-train_iter = data.Iterator(datasets=train, batch_size=128, train=True, repeat=False, device=0)
-val_iter = data.Iterator(datasets=val, batch_size=128, train=False, repeat=False, device=0)
-test_iter = data.Iterator(dataset=test, batch_size=128, train=False, sort=False, device=0)
+    return train_iter, val_iter, test_iter
